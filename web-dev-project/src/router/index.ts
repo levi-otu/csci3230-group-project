b3b9f874328524cdbc0a26fdb04095b7ce5c5585
levi-otu <levi.willms@ontariotechu.net>
2026-03-26 21:18:25 -0400

    cleaning up some boilerplate

--- web-dev-project/src/router/index.ts
+++ web-dev-project/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
